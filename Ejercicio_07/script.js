69b7f1f899b89215b72d50f7e66365c56b0ed802
RafaAgui <aguilaralvear@yahoo.es>
2024-10-18 14:10:23 +0200

    feat: corrections

--- Ejercicio_07/script.js
+++ Ejercicio_07/script.js
@@ -1,27 +1,27 @@
 function calc() {
-    let length = document.getElementById("length").value;
-    let width = document.getElementById("width").value;
+    let length = document.getElementById('length').value;
+    let width = document.getElementById('width').value;
 
     length = parseInt(length)
     width = parseInt(width)
 
-    const select = document.getElementById("select");
+    const select = document.getElementById('select');
     let calcAreaCm;
     let calcAreaInch;
 
     const calcArea = length * width;
 
-    if (select.value === "cm") {
+    if (select.value === 'cm') {
         calcAreaInch = calcArea / 2.54;
         calcAreaInch = calcAreaInch.toFixed(2);
         calcAreaCm = calcArea;
-    } else if (select.value === "inch") {
+    } else {
         calcAreaCm = calcArea * 2.54;
         calcAreaCm = calcAreaCm.toFixed(2);
         calcAreaInch = calcArea;
     }
 
-    document.getElementById("dimensions").textContent = `Las simensiones son ${length} de largo por ${width} de ancho.`;
-    document.getElementById("areaCm").textContent = calcAreaCm;
-    document.getElementById("areaInch").textContent = calcAreaInch;
+    document.getElementById('dimensions').textContent = `Las simensiones son ${length} de largo por ${width} de ancho.`;
+    document.getElementById('areaCm').textContent = calcAreaCm;
+    document.getElementById('areaInch').textContent = calcAreaInch;
 }
--- Ejercicio_08/script.js
+++ Ejercicio_08/script.js
@@ -1,6 +1,6 @@
 function calc() {
-    let persons = document.getElementById("persons").value;
-    let pizzas = document.getElementById("pizzas").value;
+    let persons = document.getElementById('persons').value;
+    let pizzas = document.getElementById('pizzas').value;
     const porciones = 8;
 
     persons = parseInt(persons)
@@ -8,11 +8,9 @@ function calc() {
 
     const totalPocions = pizzas * porciones;
     const pieces =  totalPocions / persons;
-   console.log(totalPocions, pieces)
+    const rest = totalPocions % persons;
 
-    let rest = totalPocions % persons;
-
-    document.getElementById("data").textContent = ` ${persons} para ${pizzas} pizzas.`;
-    document.getElementById("pieces").textContent = pieces;
-    document.getElementById("rest").textContent = rest;
+    document.getElementById('data').textContent = ` ${persons} para ${pizzas} pizzas.`;
+    document.getElementById('pieces').textContent = pieces;
+    document.getElementById('rest').textContent = rest;
 }
